1cbf86180c6f780d02dc2ab53df4bc7247669859
jhonnyscerni <jhonnyscerni@gmail.com>
2018-04-25 16:26:41 -0300

    Instalacao e configuracao do Font Awesome + estruturacao do layt

--- src/app/@core/shared/shared.module.ts
+++ src/app/@core/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFontAwesomeModule
   ],
   declarations: []
 })
